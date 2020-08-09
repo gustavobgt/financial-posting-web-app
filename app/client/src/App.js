2f7455d2a9f5cb5991e4f24eae2a52feed4b99f9
gustavobgt <gustavo_bgt97@hotmail.com>
2020-08-08 21:55:16 -0300

    Integrating front-end | back-end

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'api' });
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      const { data } = await api.get('/transaction?period=2020-07');
+
+      setTransactions(data.transactions);
+    };
+
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="center">Desafio Final do Bootcamp Full Stack</h1>
+
+      {transactions.map((transaction) => {
+        return <p key={transaction}>{transaction}</p>;
+      })}
+    </div>
+  );
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const transactionRouter = express.Router();
 
+transactionRouter.get('/', async (req, res) => {
+  const { query } = req;
+
+  try {
+    if (!query.period) {
+      throw new Error(
+        `É necessário informar o parâmetro "period", cujo valor deve estar no formato yyyy-mm`
+      );
+    }
+
+    res.send({
+      length: 2,
+      transactions: ['transaction1', 'transaction2'],
+    });
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
+  }
+});
+
 module.exports = transactionRouter;
